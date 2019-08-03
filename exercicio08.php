189f2207bcd3aa124c1546ae37a42a62a14ec8a2
Victor Teodoro <victor.atomo@gmail.com>
2019-08-03 12:03:42 -0300

    Add files via upload

---
+++ exercicio08.php
@@ -0,0 +1,15 @@
+<?php 
+include "header.php";
+get_header();
+
+ ?>
+<form method="POST" action="resultado.php">
+		<input type="text" name="num1">
+		<input type="text" name="num2">
+		<input type="submit" name="botao">
+	</form>
+
+	<?php get_footer(); ?>
+
+</body>
+</html>
